0861d01c032d597aa6eacabdfcace4428713db8f
Justin Lepine <lepine.justin@yahoo.com>
2022-03-28 22:05:24 -0400

    footer created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Dash from './components/Dash/Dash';
 import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.scss';
 
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route path='/' exact component={Dash}/>
       </Switch>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.scss'
+
+function Footer() {
+  return (
+    <section className='footer'>
+        <h2 className='footer__about'>About</h2>
+    </section>
+  )
+}
+
+export default Footer;
