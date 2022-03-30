9ffe8f602836330b77ea83d6f09e0eda480d611d
Justin Lepine <lepine.justin@yahoo.com>
2022-03-29 21:21:56 -0400

    crankbait page styled with mock data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Dash from './components/Dash/Dash';
 import Lures from './components/Lures/Lures';
 import Tackle from './components/Tackle/Tackle';
 import Rods from './components/Rods/Rods';
+import Crankbait from './components/Crankbait/Crankbait';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -16,6 +17,7 @@ function App() {
         <Route path='/lures' component={Lures}/>
         <Route path='/tackle' component={Tackle}/>
         <Route path='/rods' component={Rods}/>
+        <Route path='/crankbait' component={Crankbait}/>
       </Switch>
       <Footer />
     </Router>
---
+++ src/components/Crankbait/Crankbait.js
@@ -0,0 +1,35 @@
+import React from "react";
+import CrankbaitIcon from "../../assets/icons/crankbait.svg";
+import "./Crankbait.scss";
+
+function Crankbait() {
+  return (
+    <section className="crankbait">
+      <div className="crankbait__top">
+        <h1 className="crankbait__title">Crankbait</h1>
+        <img className="crankbait__icon" src={CrankbaitIcon} alt="crankbait icon" />
+      </div>
+      <p>
+        Crankbait is a fishing lure often made from hard ABS plastic, some have
+        metal bearings inside to create noise and attract predator fish. The
+        bait usually takes shape of a small fish, and or an injured fish. Most
+        crankbaits include a diving bill to dive the bait lower into the water,
+        longer the bill deeper they dive when reeling in.
+      </p>
+      <div className="crankbait__inv">
+        <div className="crankbait__subtitles">
+          <h4>Name</h4>
+          <h4>Depth</h4>
+          <h4>Target</h4>
+        </div>
+        <ul className="crankbait__list">
+          <li>Shad Rap</li>
+          <li>4' - 7'</li>
+          <li>Bass</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default Crankbait;
