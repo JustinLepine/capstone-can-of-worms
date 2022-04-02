ed31afb45ca37dbafb6d9c9fb472268252cbb765
Justin Lepine <lepine.justin@yahoo.com>
2022-04-02 11:20:29 -0400

    added GET and POST axios requests

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,9 +1,20 @@
 import axios from 'axios';
+import { useHistory } from "react-router-dom";
+
+const history = useHistory()
 
 const tools = {
 
     getInv: () => axios.get('/inventory'),
 
-    addInv: () => axios.post('/inventory')
-
+    addInv: () => axios
+        .post('/inventory', {
+            title: e.target.inputTitle.value,
+            depth: e.target.inputDepth.value,
+            target: e.target.inputTarget.value
+    })
+    .then(res => {
+        console.log(res.data.id)
+        history.push(`/`)
+    })
 }
