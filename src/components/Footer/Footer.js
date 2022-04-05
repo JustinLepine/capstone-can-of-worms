c0d69a6842b8dde811b9567d5d320e2faa827eec
Justin Lepine <lepine.justin@yahoo.com>
2022-04-05 13:14:05 -0400

    footer links created

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import './Footer.scss'
+import React from "react";
+import GithubIcon from "../../assets/icons/github-icon.svg";
+import LinkedInIcon from "../../assets/icons/linkedin-icon.svg";
+import "./Footer.scss";
 
 function Footer() {
   return (
-    <section className='footer'>
-        <h2 className='footer__about'>About</h2>
+    <section className="footer">
+      <a href="https://github.com/JustinLepine">
+        <img className="footer__git-icon" src={GithubIcon} alt="icon" />
+      </a>
+      <a href="https://linkedin.com/in/justin-lepine">
+        <img className="footer__link-icon" src={LinkedInIcon} alt="icon" />
+      </a>
     </section>
-  )
+  );
 }
 
 export default Footer;
