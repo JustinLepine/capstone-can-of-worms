a210e87ad21c10958cddb0775cc0422cdae11e02
Justin Lepine <lepine.justin@yahoo.com>
2022-07-31 14:52:17 -0400

    cleaned up scss and jsx

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,11 @@
 import { motion } from "framer-motion";
 import React, { useState } from 'react';
+import { API_URL } from '../../config/index.js';
 import "./Login.scss"
 
 const Login = () => {
-    // const [isSignup, _setIsSignup] = useState(false);
     const [isSignup, setIsSignup] = useState(true);
-    console.log(isSignup)
+    console.log(API_URL)
 
     const resetInputField = () => {
         document.getElementById('form').reset();
@@ -14,33 +14,27 @@ const Login = () => {
         const stateToggle = (e) => {
             e.preventDefault();
             setIsSignup(!isSignup);
-    };
-      
+    };     
 
   return (
     <motion.section className="login">
-        {isSignup ? <h1 className="login__title">Create User</h1> : <h1 className="login__title">Login</h1>}
+        <h1 className="login__title">{isSignup ? 'Create User' : 'Login'}</h1>
         <form id="form" className="login__form">
             <h5 className="login__subtitle">USER NAME</h5>
-            <input className="login__input" name="inputName"></input>
+            <input type="text" placeholder="Enter name" className="login__input" name="name" required></input>
             <h5 className="login__subtitle">PASSWORD</h5>
-            <input className="login__input" name="inputName"></input>
-            {isSignup && (
-                <>
-                    <h5 className="login__subtitle">EMAIL</h5>
-                    <input className="login__input" name="inputName"></input>
-                </>                
-            )}
-
+            <input type="password" placeholder="Enter password" className="login__input" name="password" required></input>
+            {isSignup && (<>
+                <h5 className="login__subtitle">CONFIRM PASSWORD</h5>
+                <input type="password" placeholder="Enter password" className="login__input" name="confirmPassword" required></input>
+                <h5 className="login__subtitle">EMAIL</h5>
+                <input type="email" placeholder="Enter password" className="login__input" name="email" required></input>
+            </>)}
             <div className="login__button-box">
-                <button type="button" onClick={resetInputField} className="login__button">RESET</button>
-                <button 
-                // type="submit"
-                 onClick={stateToggle} className="login__button">ADD</button>
+                <button type="button" onClick={resetInputField} className="login__button">CLEAR</button>
+                <button type="submit" onClick={stateToggle} className="login__button">{isSignup ? 'CREATE' : 'LOGIN'}</button>
             </div>
         </form>
-
-
     </motion.section>
   )
 }
